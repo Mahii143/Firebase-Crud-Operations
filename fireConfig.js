59a43623115fe0d49a2a3d0af39c27057eeca591
Mahii143 <2012091@nec.edu.in>
2023-04-20 14:56:21 +0530

    initial

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXnaxCNOuf6rXHtNDKgEzfoqWCcW-xsvI",
+    authDomain: "editable-table-ec1cd.firebaseapp.com",
+    databaseURL: "https://editable-table-ec1cd-default-rtdb.firebaseio.com",
+    projectId: "editable-table-ec1cd",
+    storageBucket: "editable-table-ec1cd.appspot.com",
+    messagingSenderId: "901686646262",
+    appId: "1:901686646262:web:daa1c18bb6a01678d9e3d7"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
